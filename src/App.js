e01df0b427597c91d19b53d670fd3e75fccb409c
Magne Svarverud Skaro <magneskaro@gmail.com>
2024-03-09 16:55:29 +0100

    setState adds etc.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const [omega6, setOmega6] = useState(0)
   const [mettet, setMettet] = useState(0)
   const [transfett, setTransfett] = useState(0)
-  const [kolesterol, setkolesteros] = useState(0)
+  const [kolesterol, setKolesterol] = useState(0)
   
   const [cystin, setCystin] = useState(0)
   const [histidin, setHistidin] = useState(0)
@@ -29,7 +29,7 @@ function App() {
   const [lysin, setLysin] = useState(0)
   const [metionin, setMetionin] = useState(0)
   const [fenylalanin, setFenylalanin] = useState(0)
-  const [trenonin, setTrenonin] = useState(0)
+  const [treonin, setTreonin] = useState(0)
   const [tryptofan, setTryptofan] = useState(0)
   const [tyrosin, setTyrosin] = useState(0)
   const [valin, setValin] = useState(0)
@@ -65,7 +65,48 @@ function App() {
     setStivelse((stivelse) => stivelse + newFood.carbohydrates[0].starch)
     setSukker((sukker) => sukker + newFood.carbohydrates[0].sugars)
 
-    setFiber((fiber) => fiber + newFood.carbohydrates[0].fiber)
+    setEnumettet((enumettet) => enumettet + newFood.lipids[0].fat[0].monounsaturated)
+    setOmega3((omega3) => omega3 + newFood.lipids[0].fat[0].polyunsaturated[0].omega3)
+    setOmega6((omega6) => omega6 + newFood.lipids[0].fat[0].polyunsaturated[0].omega6)
+    setMettet((mettet) => mettet + newFood.lipids[0].fat[0].saturated)
+    setTransfett((transfett) => transfett + newFood.lipids[0].fat[0].transFats)
+    setKolesterol((kolesterol) => kolesterol + newFood.lipids[0].cholesterol)
+    
+    setCystin((cystin) => cystin + newFood.protein[0].cystine)
+    setHistidin((histidin) => histidin + newFood.protein[0].histidine)
+    setIsoleucin((isoleucin) => isoleucin + newFood.protein[0].isoleucine)
+    setLeucin((leucin) => leucin + newFood.protein[0].leucine)
+    setLysin((lysin) => lysin + newFood.protein[0].lysine)
+    setMetionin((metionin) => metionin + newFood.protein[0].methionine)
+    setFenylalanin((fenylalanin) => fenylalanin + newFood.protein[0].phenylalanine)
+    setTreonin((treonin) => treonin + newFood.protein[0].threonine)
+    setTryptofan((tryptofan) => tryptofan + newFood.protein[0].tryptophan)
+    setTyrosin((tyrosin) => tyrosin + newFood.protein[0].tyrosine)
+    setValin((valin) => valin + newFood.protein[0].valine)
+
+    setB1((b1) => b1 + newFood.vitamins[0].b1)
+    setB2((b2) => b2 + newFood.vitamins[0].b2)
+    setB3((b3) => b3 + newFood.vitamins[0].b3)
+    setB5((b5) => b5 + newFood.vitamins[0].b5)
+    setB6((b6) => b6 + newFood.vitamins[0].b6)
+    setB12((b12) => b12 + newFood.vitamins[0].b12)
+    setFolsyre((folsyre) => folsyre + newFood.vitamins[0].folate)
+    setAVitamin((aVitamin) => aVitamin + newFood.vitamins[0].vitamin_a)
+    setCVitamin((cVitamin) => cVitamin + newFood.vitamins[0].vitamin_c)
+    setDVitamin((dVitamin) => dVitamin + newFood.vitamins[0].vitamin_d)
+    setEVitamin((eVitamin) => eVitamin + newFood.vitamins[0].vitamin_e)
+    setKVitamin((kVitamin) => kVitamin + newFood.vitamins[0].vitamin_k)
+
+    setKalsium((kalsium) => kalsium + newFood.minerals[0].calcium)
+    setKobber((kobber) => kobber + newFood.minerals[0].copper)
+    setJern((jern) => jern + newFood.minerals[0].iron)
+    setMagnesium((magnesium) => magnesium + newFood.minerals[0].magnesium)
+    setMangan((mangan) => mangan + newFood.minerals[0].manganese)
+    setFosfor((fosfor) => fosfor + newFood.minerals[0].phosphorus)
+    setKalium((kalium) => kalium + newFood.minerals[0].potassium)
+    setSelen((selen) => selen + newFood.minerals[0].selenium)
+    setNatrium((natrium) => natrium + newFood.minerals[0].sodium)
+    setSink((sink) => sink + newFood.minerals[0].zinc)
   }
 
   function handleRemove(name) {
@@ -107,7 +148,7 @@ function App() {
           lysin={lysin}
           metionin={metionin}
           fenylalanin={fenylalanin}
-          trenonin={trenonin}
+          treonin={treonin}
           tryptofan={tryptofan}
           tyrosin={tyrosin}
           valin={valin}
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,49 @@
 import './grid.css'
 
-export default function Grid({ fiber, setFiber, stivelse, setStivelse, sukker, setSukker }) {
+export default function Grid({
+        fiber,
+        stivelse,
+        sukker,
+        enumettet,
+        omega3,
+        omega6,
+        mettet,
+        transfett,
+        kolesterol,
+        cystin,
+        histidin,
+        isoleucin,
+        leucin,
+        lysin,
+        metionin,
+        fenylalanin,
+        treonin,
+        tryptofan,
+        tyrosin,
+        valin,
+        b1,
+        b2,
+        b3,
+        b5,
+        b6,
+        b12,
+        folsyre,
+        aVitamin,
+        cVitamin,
+        dVitamin,
+        eVitamin,
+        kVitamin,
+        kalsium,
+        kobber,
+        jern,
+        magnesium,
+        mangan,
+        fosfor,
+        kalium,
+        selen,
+        natrium,
+        sink
+    }) {
     return(
         <div className='gridContainer'>
             <div className="grid">
@@ -29,86 +72,86 @@ export default function Grid({ fiber, setFiber, stivelse, setStivelse, sukker, s
                         <tbody>
                             <tr>
                                 <td>Fett</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'></td>
                             </tr>
                             <tr>
                                 <td className='subList' style={{paddingLeft: '20px'}}>Enumettet</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{enumettet}</td>
                             </tr>
                             <tr className='subList'>
                                 <td style={{paddingLeft: '20px'}}>Flerumettet</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'></td>
                             </tr>
                             <tr className='subSubList'>
                                 <td style={{paddingLeft: '30px'}}>Omega-3</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{omega3}</td>
                             </tr>
                             <tr className='subSubList'>
                                 <td style={{paddingLeft: '30px'}}>Omega-6</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{omega6}</td>
                             </tr>
                             <tr className='subList'>
                                 <td style={{paddingLeft: '20px'}}>Mettet</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{mettet}</td>
                             </tr>
                             <tr className='subList'>
                                 <td style={{paddingLeft: '20px'}}>Transfett</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{transfett}</td>
                             </tr>
                             <tr>
                                 <td>Kolesterol</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{kolesterol}</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
                 <div className='tableContainer'>
-                    <h5 className='tableHeader'>Test</h5>
+                    <h5 className='tableHeader'>Proteiner</h5>
                     <table className="foods-table">
                         <tbody>
                             <tr>
                                 <td>Cystin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{cystin}</td>
                             </tr>
                             <tr>
                                 <td>Histidin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{histidin}</td>
                             </tr>
                             <tr>
                                 <td>Isoleucin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{isoleucin}</td>
                             </tr>
                             <tr>
                                 <td>Leucin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{leucin}</td>
                             </tr>
                             <tr>
                                 <td>Lysin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{lysin}</td>
                             </tr>
                             <tr>
                                 <td>Metionin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{metionin}</td>
                             </tr>
                             <tr>
                                 <td>Fenylalanin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{fenylalanin}</td>
                             </tr>
                             <tr>
                                 <td>Trenonin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{treonin}</td>
                             </tr>
                             <tr>
                                 <td>Tryptofan</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{tryptofan}</td>
                             </tr>
                             <tr>
                                 <td>Tyrosin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{tyrosin}</td>
                             </tr>
                             <tr>
                                 <td>Valin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{valin}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -121,51 +164,51 @@ export default function Grid({ fiber, setFiber, stivelse, setStivelse, sukker, s
                         <tbody>
                             <tr>
                                 <td>B1</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{b1}</td>
                             </tr>
                             <tr>
                                 <td>B2</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{b2}</td>
                             </tr>
                             <tr>
                                 <td>B3</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{b3}</td>
                             </tr>
                             <tr>
                                 <td>B5</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{b5}</td>
                             </tr>
                             <tr>
                                 <td>B6</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{b6}</td>
                             </tr>
                             <tr>
                                 <td>B12</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{b12}</td>
                             </tr>
                             <tr>
                                 <td>Folsyre</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{folsyre}</td>
                             </tr>
                             <tr>
                                 <td>A-vitamin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{aVitamin}</td>
                             </tr>
                             <tr>
                                 <td>C-vitamin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{cVitamin}</td>
                             </tr>
                             <tr>
                                 <td>D-vitamin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{dVitamin}</td>
                             </tr>
                             <tr>
                                 <td>E-vitamin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{eVitamin}</td>
                             </tr>
                             <tr>
                                 <td>K-vitamin</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{kVitamin}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -176,43 +219,43 @@ export default function Grid({ fiber, setFiber, stivelse, setStivelse, sukker, s
                         <tbody>
                             <tr>
                                 <td>Kalsium</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{kalsium}</td>
                             </tr>
                             <tr>
                                 <td>Kobber</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{kobber}</td>
                             </tr>
                             <tr>
                                 <td>Jern</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{jern}</td>
                             </tr>
                             <tr>
                                 <td>Magnesium</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{magnesium}</td>
                             </tr>
                             <tr>
                                 <td>Mangan</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{mangan}</td>
                             </tr>
                             <tr>
                                 <td>Fosfor</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{fosfor}</td>
                             </tr>
                             <tr>
                                 <td>Kalium</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{kalium}</td>
                             </tr>
                             <tr>
                                 <td>Selen</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{selen}</td>
                             </tr>
                             <tr>
                                 <td>Natrium</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{natrium}</td>
                             </tr>
                             <tr>
                                 <td>Sink</td>
-                                <td className='right-cell'>0</td>
+                                <td className='right-cell'>{sink}</td>
                             </tr>
                         </tbody>
                     </table>
